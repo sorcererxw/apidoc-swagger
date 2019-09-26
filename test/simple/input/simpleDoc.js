6879a2e204ce9aa270216dfa0c57a05f9c2a5ee0
sorcererxw <tujipqy@gmail.com>
2019-09-26 11:16:34 +0800

    Fixing description P-tag

--- test/simple/input/simpleDoc.js
+++ test/simple/input/simpleDoc.js
@@ -2,7 +2,7 @@
  * @api {get} /user/id Request User information
  * @apiName GetUser
  * @apiGroup User
- *
+ * @apiDescription Request User information
  * @apiParam {Number} id Users unique ID.
  *
  * @apiSuccess {String} firstname Firstname of the User.
